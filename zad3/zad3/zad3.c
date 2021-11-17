f305bf0951d3ea91369e2ff22af82c83f11580f0
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-11-17 21:34:54 +0100

    Task 2

--- zad3/zad3/zad3.c
+++ zad3/zad3/zad3.c
@@ -13,4 +13,15 @@ int main()
 	for (int i = 0; i < 10; i++) {
 		printf("%d\n", tab1[i]);
 	}
+	
+	printf("\n--ZAD2--");
+	int tab2[10];
+	for (int i = 0, j = 3; i < 10; i++, j += 3) {
+		tab2[i] = j;
+	}
+	for (int i = 0; i < 10; i++) {
+		if (i % 2 == 0) {
+			printf("%d\n", tab2[i]);
+		}
+	}
 }
