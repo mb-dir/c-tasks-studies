17ff5d71f303eb41caebd5abb632c9c5e9e49bce
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-11-17 21:29:09 +0100

    Task 1

---
+++ zad3/zad3/zad3.c
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <Windows.h>
+
+int main()
+{
+	printf("--ZAD1--");
+	int tab1[10];
+	for (int i = 0, j = 3; i < 10; i++, j += 3) {
+		tab1[i] = j;
+	}
+	for (int i = 0; i < 10; i++) {
+		printf("%d\n", tab1[i]);
+	}
+}
