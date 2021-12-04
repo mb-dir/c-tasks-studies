93df5f4c01470c0065addc0123e5e2dfdd403dd5
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-12-04 16:12:41 +0100

    task2

--- zad4/zad4/zad4.c
+++ zad4/zad4/zad4.c
@@ -20,4 +20,27 @@ int main()
 	printf("%s",strcat(t1, t2));
 
 	printf("\ndlugosc zlaczanego tekstu: %d", strlen(t1));
+
+	//zad2
+	
+	for (int i = 97; i < 123; i++) {
+		if(i==97) {
+			printf("znak - kod\n");
+		}
+		printf("%c - %d\n", i, i);
+	}printf("\n\n");
+
+	for (int i = 65; i < 91; i++) {
+		if (i == 65) {
+			printf("znak - kod\n");
+		}
+		printf("%c - %d\n", i, i);
+	}printf("\n\n");
+
+	for (int i = 48; i < 58; i++) {
+		if (i == 48) {
+			printf("znak - kod\n");
+		}
+		printf("%c - %d\n", i, i);
+	}
 }
