a2adcadc667700b25b8d8d28a0de3d17378cf004
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-12-15 12:25:44 +0100

    Basics

---
+++ zad5/zad5/zad5.c
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <Windows.h>
+#include <string.h>
+#include "test.h"
+
+//funkcje
+
+int main()
+{
+    printf("%d", suma(41,6));
+}
+
