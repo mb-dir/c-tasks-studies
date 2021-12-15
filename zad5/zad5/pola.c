74bebf23ed2a69877b6fff24d3376d6278b70049
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-12-15 12:38:34 +0100

    Error handling

--- zad5/zad5/pola.c
+++ zad5/zad5/pola.c
@@ -1,7 +1,19 @@
 #pragma once
+void error() {
+ printf("Zle dane");
+}
+
 float kolo(float r, float pi) {
+	if (r < 0 || pi < 0) {
+		error();
+		return 0;
+	}
 	return r*r * pi;
 }
 float kula(float r, float pi) {
+	if (r < 0 || pi < 0) {
+		error();
+		return 0;
+	}
 	return (4 / 3) * pi * r * r * r;
 }
--- zad5/zad5/pola.h
+++ zad5/zad5/pola.h
@@ -1,3 +1,6 @@
 #pragma once
+
+void error();
+
 float kolo(float r, float pi);//To tylko deklaracje
 float kula(float r, float pi);
