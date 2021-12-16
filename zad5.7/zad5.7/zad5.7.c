4a03d81bbaf9177ad6dbb70324c079ab87eb3182
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-12-16 21:59:09 +0100

    Done task

---
+++ zad5.7/zad5.7/zad5.7.c
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+int silnia(int a);
+
+int main() {
+	int value = silnia(6);
+	printf("%d", value);
+	return 0;
+}
+
+int silnia(int a) {
+	//1! == 1, 0!==1
+	if (a <=1) {
+		return 1;
+	}
+	else {
+		return a * silnia(a - 1);
+	}
+}
