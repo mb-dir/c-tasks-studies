2cc443b158b96adfbbdf9dc77ddb87b83029a3fc
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-12-16 22:08:55 +0100

    Done task

---
+++ zad5.8/zad5.8/zad5.8.c
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+float ciag(float a);
+
+int main() {
+	float value = ciag(3);
+	printf("%f", value);
+	return 0;
+}
+
+float ciag(float a) {
+	if (a == 1) {
+		return 1;
+	}
+	else if (a == 2) {
+		return 0.5;
+	}
+	else {
+		return -1 * ciag(a - 1) * ciag(a - 2);
+	}
+}
