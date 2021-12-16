556c147b497f0bff970eb62ccd2d8f3fda85fe3b
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-12-16 21:49:33 +0100

    Fibonacci recursion algorythm

---
+++ zad5.6/zad5.6/zad5.6.c
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+int fib(int a);
+
+int main() {
+	int value = fib(6);
+	printf("%d", value);
+	return 0;
+}
+
+int fib(int a) {
+	if (a == 0) {
+		return 0;
+	}else if(a == 1){
+		return 1;
+	}
+	else {
+		return fib(a - 2) + fib(a - 1);
+	}
+}
