755579f76b2ea464dbfdd3269b32116444af37d5
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-12-17 22:39:38 +0100

    Done taks

---
+++ zad5.5/zad5.5/zad5.5.c
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+int nwd(int a, int b);
+
+int main() {
+	int value = nwd(19,48);
+	printf("%d", value);
+	return 0;
+}
+int nwd(int a, int b) {
+	if (a == b) {
+		return a;
+	}
+	else {
+		if (a > b) {
+			return nwd(a-b, b);
+		}
+		else {
+			return nwd(a, b-a);
+		}
+	}
+}
