eb7669271d4838d9679da1c4b21200308d849f9b
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2021-12-17 22:44:22 +0100

    Done taks

---
+++ zad5.4/zad5.4/zad5.4.c
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+void toBin(int a);
+
+int main() {
+	toBin(19);
+	return 0;
+}
+
+void toBin(int a) {
+	if (a!=0) {
+		toBin(a/2);
+		printf("%d", a%2);
+	}
+}
