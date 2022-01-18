20ef907cd8ac0c15289ab0c449ec4f1a5ff936a0
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2022-01-18 20:39:46 +0100

    v1

---
+++ prepForExam/prepForExam/prepForExam.c
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <Windows.h>
+
+int main() {
+	//Random numbers
+	srand(time(NULL));
+	
+	for (int i = 0; i < 15; i++) {
+		int liczba = rand() % 5-1;
+		printf("%d\n", liczba);
+	}
+	return 0;
+}
