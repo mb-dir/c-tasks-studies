f6a9c799fcf089c73170de706155690eb27b100c
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2022-01-18 20:55:48 +0100

    v2

--- prepForExam/prepForExam/prepForExam.c
+++ prepForExam/prepForExam/prepForExam.c
@@ -2,14 +2,30 @@
 #include <stdlib.h>
 #include <time.h>
 #include <Windows.h>
+#include <stdbool.h>
 
 int main() {
 	//Random numbers
-	srand(time(NULL));
+	/*srand(time(NULL));
 	
 	for (int i = 0; i < 15; i++) {
 		int liczba = rand() % 5-1;
 		printf("%d\n", liczba);
+	}*/
+
+	//Draw christmas tree
+	for (int i = 0; i < 10; i++) {
+		for (int j = 10; j > i; j--) {
+			printf(" ");
+		}
+		for (int j = 0; j <= i; j++) {
+			printf("**");
+		}
+		for (int j = 10; j > i; j--) {
+			printf(" ");
+		}
+		printf("\n");
 	}
+
 	return 0;
 }
