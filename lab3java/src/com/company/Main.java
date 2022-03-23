d7debbba7a7365be3cefaf0eff73ede6bee2655e
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2022-03-23 21:59:35 +0100

    Tasks 3 and 4

--- lab3java/src/com/company/Main.java
+++ lab3java/src/com/company/Main.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
@@ -9,12 +10,17 @@ public class Main {
 
         //zad1
         int n;
-        System.out.println("Podaj liczbe studentów");
-        n=in.nextInt();
-        zad1(n);
+        //System.out.println("Podaj liczbe studentów");
+        //n=in.nextInt();
+        //zad1(n);
 
         //zad2
-        zad2();
+        //zad2();
+
+        //zad3
+        //zad3();
+
+        zad4();
     }
 
     public static void zad1(int n){
@@ -54,5 +60,38 @@ public class Main {
         System.out.println("Ilość dodatnich: " + ileD);
         System.out.println("Suma ujemnych: " + sumaU);
         System.out.println("Ilość ujemnych: " + ileU);
+    }
+
+    public static void zad3(){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Podan n");
+        int n = in.nextInt();
+        int sumaP = 0;
+
+        for(int i = 0; i < n; i++){
+            System.out.println("Podaj wyraz ciągu");
+            int w = in.nextInt();
+            if(w %2 == 0){
+                sumaP +=w;
+            }
+        }
+        System.out.println("Suma parzystych: " + sumaP);
+    }
+
+    public static void zad4(){
+        Random rand = new Random();
+        Scanner in = new Scanner(System.in);
+        System.out.println("Podan n");
+        int n = in.nextInt();
+        int sumaP = 0;
+
+        for(int i = 0; i < n; i++){
+            int a = rand.nextInt(56) - 10;
+            System.out.println(a);
+            if(a%2==0){
+                sumaP+=a;
+            }
+        }
+        System.out.println("Suma parzystych wyrazów to: " + sumaP);
     }
 }
