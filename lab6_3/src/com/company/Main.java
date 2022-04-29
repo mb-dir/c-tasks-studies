c871976153fa75db62bf1b60d675a8fd340bc75b
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2022-04-30 00:00:40 +0200

    Done task

--- lab6_3/src/com/company/Main.java
+++ lab6_3/src/com/company/Main.java
@@ -4,11 +4,34 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        Ksiazka k = new Ksiazka("Dupa", "Karol dupa", 1992, 78);
+        //Tworzenie przykładowych obiektów
+        Ksiazka k = new Ksiazka("Kadlilak", "Karol", 1992, 78);
         Powiesc p = new Powiesc("Lalka", "Nie wiem", 1231, 999, true);
         Podrecznik podr = new Podrecznik("Bliżej świata", "Wsip", 2002, 876, "WF", 5);
         Ksiegarnia ksiegarnia = new Ksiegarnia(2137, 9, 19);
 
+        //Przykład na rzutowanie
+        //Książka nie jest referencją Powieści
+        if(k instanceof Powiesc){
+            System.out.println("Tak");
+        }else{
+            System.out.println("Nie");
+        }
+        //Książka zrzutowana na powieść jest referencją Powieści
+        Ksiazka k1 = (Powiesc) p;
+        if(k1 instanceof Powiesc){
+            System.out.println("Tak");
+        }else{
+            System.out.println("Nie");
+        }
+        System.out.println();
+
+        //Sprawdzenie działania części aplikacji powiązanej z klasą Książka i jej pochodnymi
+        p.opis();
+
+        System.out.println();
+
+        //Działanie księgarni
         ksiegarnia.opis();
     }
 }
